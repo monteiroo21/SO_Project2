172890a74ca208644f0e37e4d36fefdc1e0bfb73
jpapinto <jpapinto@ua.pt>
2023-12-26 17:24:28 +0000

    Chef

---
+++ semaphore_restaurant/run/test.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# comando para remover todos os segmentos de memória compartilhada no Unix-like systems
+ipcrm -a
+
+# Navega para o diretório src
+cd ../src
+
+# Verifica se há argumentos
+if [ $# -eq 0 ]; then
+    # Se não houver argumentos, executa make all_bin
+    make all_bin
+else
+    # Se houver argumentos, executa make com o primeiro argumento
+    make "$1"
+fi
+
+# Navega para o diretório run
+cd ../run
+
+# Executa o programa probSemSharedMemRestaurant
+./probSemSharedMemRestaurant
+
+
